d1b2ee38eca51976688b1580579c5aec1e0c6813
Nils Fahle <nils.fahle@marlamalde.group>
2023-07-11 11:33:30 +0200

    color messup

--- src/game/board/Board.ts
+++ src/game/board/Board.ts
@@ -15,21 +15,13 @@ export class Board {
   }
 
   generateBoard() {
-    // generate empty board
-    // board is a hexagon grid
-    // represented by axial coordinates
-
-    // this.fields.set({q: 0, r: 0}, new Field({q: 0, r: 0}, this.app))
-
     for (let q = -BOARD_SIZE; q <= BOARD_SIZE; q++) {
       const r1 = Math.max(-BOARD_SIZE, -q - BOARD_SIZE);
       const r2 = Math.min(BOARD_SIZE, -q + BOARD_SIZE);
       for (let r = r1; r <= r2; r++) {
-        const field = new Field({ q, r }, this.app);
+        const field = new Field({ q, r }, this.app, q + BOARD_SIZE, r + r1);
         this.fields.set(field.coordinate, field);
       }
     }
-
-    // console.log(this.fields);
   }
 }
--- src/game/board/Field.ts
+++ src/game/board/Field.ts
@@ -1,4 +1,4 @@
-import { Application, Graphics, Point, Sprite } from "pixi.js";
+import { Application, Graphics, Point, Sprite, Text } from "pixi.js";
 import { BoardColor, getColor } from "./BoardColor";
 import { Coordinate } from "./Coordinate";
 import { Piece } from "./Piece";
@@ -9,16 +9,20 @@ export const HEX_SIZE = 40;
 export const HEX_WIDTH = HEX_SIZE * 2;
 export const HEX_HEIGHT = HEX_WIDTH * 0.8660254;
 
+const COLOR_ORDER = [BoardColor.White, BoardColor.Gray, BoardColor.Black];
+
 export class Field {
   public piece: Piece | null = null;
   public color: BoardColor;
   public coordinate: Coordinate;
   public sprite: Sprite;
   private offset: Point;
-  private hex_size: number;
+  private hexSize: number;
+  private colIndex;
+  private colPosition;
 
   private get hex_width() {
-    return this.hex_size * 2;
+    return this.hexSize * 2;
   }
 
   private get hex_height() {
@@ -27,23 +31,28 @@ export class Field {
 
   private app: Application;
 
-  /**
-   *
-   */
-  constructor(coordinate: Coordinate, app: Application) {
+  constructor(
+    coordinate: Coordinate,
+    app: Application,
+    colOffset: number,
+    colPosition: number
+  ) {
     this.coordinate = coordinate;
-    this.color = this.getColorByCoordinate(coordinate);
+    this.color = this.calculateFieldColor(coordinate);
     this.app = app;
     this.offset = new Point(app.view.width / 2, app.view.height / 2);
+    this.colIndex = colOffset;
+    this.colPosition = colPosition;
 
-    this.hex_size = (app.view.width - 50) / (9 * 2 + 1);
+    this.hexSize = (app.view.width - 50) / (9 * 2 + 1);
 
     this.sprite = this.getHexagonSprite();
     this.drawHexagon();
   }
 
-  private getColorByCoordinate(_coordinate: Coordinate) {
-    return BoardColor.Gray;
+  private calculateFieldColor(_coordinate: Coordinate) {
+    const index = (this.colIndex + this.colPosition) % COLOR_ORDER.length;
+    return COLOR_ORDER[index];
   }
 
   private getHexagonSprite() {
@@ -59,27 +68,37 @@ export class Field {
 
   private drawHexagon() {
     const hex = new Graphics();
+    const coordinateAsString = `${this.coordinate.q},${this.coordinate.r}`;
+    const text = new Text(coordinateAsString);
 
     const layout = new Layout(
       Layout.flat,
-      new Point(this.hex_size, this.hex_size),
+      new Point(this.hexSize, this.hexSize),
       this.offset
     );
 
     const corners = layout.polygonCorners(this.coordinate);
     const coordinates = corners.map((corner) => [corner.x, corner.y]).flat(1);
 
-    hex.beginFill(0xffffff);
+    console.log(this.getHexColor())
+
+    hex.beginFill(this.getHexColor());
     hex.lineStyle(2, 0x000000, 1);
     hex.drawPolygon(coordinates);
     hex.endFill();
 
     this.app.stage.addChild(hex);
 
+    // center of hex
+    const center = layout.hexToPixel(this.coordinate);
+    text.anchor.set(0.5);
+    text.position = center;
+    this.app.stage.addChild(text);
+
     return hex;
   }
 
   private getHexColor = () => {
     return getColor(this.color);
-  }
+  };
 }
